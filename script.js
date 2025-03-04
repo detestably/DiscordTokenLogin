46bd9fefdc3a07ec252be545e0dd79f7cd64e0e0
detestably <66346123+detestably@users.noreply.github.com>
2025-03-04 03:38:40 -0300

    Script para login usando token do Discord.

---
+++ script.js
@@ -0,0 +1,7 @@
+function login(token) {
+    setInterval(() => {
+        document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.token = `"${token}"`;
+    }, 50);
+    setTimeout(() => { location.reload(); }, 2500);
+}
+login('TOKEN');
